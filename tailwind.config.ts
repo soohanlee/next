1590b013e17e0e53747b1e7758a251be16d2b91d
famoz-soohan <68622934+famoz-soohan@users.noreply.github.com>
2024-02-02 16:47:02 +0900

    ✨ next.js 페이지 예시 세팅

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,22 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: "#1967b0", // 메인 컬러
+        secondary: "#a8a8a8", // 서브 컬러
+        tertiary: "#848484", // 추가 서브 컬러
+        white: "#ffffff", // 흰색
+        dark: "#222222", // 매우 어두운 색, 주로 텍스트나 배경에 사용
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      // 필요에 따라 theme의 다른 부분을 확장할 수 있습니다.
     },
   },
   plugins: [],
 };
 export default config;
+
